ccd3c8697ef30f0270f091b1a1ff1476afd0db47
FcnAshir <160272348+FcnAshir@users.noreply.github.com>
2024-04-01 02:51:28 +0500

    NumberGuessinggame

---
+++ new.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+console.log("Welcome To Syed Ashir Game")
+const random = Math.floor(Math.random()*9 +1)
+const answer = await inquirer.prompt([{
+name:"AShir",
+type:"number",
+message:"Please guess the number between 1_9"
+
+}]);
+if(answer.AShir===random){
+console.log("Congratulation you guess a write number")
+}else{
+console.log("You guess a wrong number ")
+};
